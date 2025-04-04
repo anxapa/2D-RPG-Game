bbc6b2593bbf67786c68bb7c159b7d8ecb14bcab
anxapa <rapidzen16@gmail.com>
2025-04-04 16:31:14 -0700

    Added camera controller script
    - Added camera controller script with bounds.

--- Assets/Scripts/CameraMotor.cs
+++ Assets/Scripts/CameraMotor.cs
@@ -3,6 +3,44 @@ using UnityEngine;
 public class CameraMotor : MonoBehaviour
 {
     [SerializeField] private Transform lookAt;
-    private float boundX = 0.15f;
-    private float boundY = 0.05f;
+    [SerializeField] private float boundX = 0.15f;
+    [SerializeField] private float boundY = 0.05f;
+
+    private void LateUpdate()
+    {
+        Vector3 delta = Vector3.zero;
+
+        // Checks if the player is outside of the bounds
+        float deltaX = lookAt.position.x - transform.position.x;
+        if (Mathf.Abs(deltaX) > boundX)
+        {
+            // Player is on the right of the bound.
+            if (deltaX > 0)
+            {
+                delta.x = deltaX - boundX;
+            }
+            // Player is on the left of the left.
+            else
+            {
+                delta.x = deltaX + boundX;
+            }
+        }
+
+        float deltaY= lookAt.position.y - transform.position.y;
+        if (Mathf.Abs(deltaY) > boundY)
+        {
+            // Player is on the top of the bound.
+            if (deltaY > 0)
+            {
+                delta.y = deltaY - boundY;
+            }
+            // Player is on the bottom of the bound.
+            else
+            {
+                delta.y = deltaY + boundY;
+            }
+        }
+
+        transform.position += delta;
+    }
 }
