2bcc85f2e2a62030e56492e524b8fa6ea0ccd2ee
anxapa <rapidzen16@gmail.com>
2025-04-04 16:13:09 -0700

    Added basic player controls
    - Added basic player controls.
    - Added basic collision system using raycasting.

---
+++ Assets/Scripts/CameraMotor.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+    [SerializeField] private Transform lookAt;
+    private float boundX = 0.15f;
+    private float boundY = 0.05f;
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    BoxCollider2D boxCollider;
+    Vector3 moveDelta;
+    RaycastHit2D hit;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Input handling
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        // Resetting moveDelta
+        moveDelta = new Vector3(x, y, 0);
+
+        // Changing sprite to direction
+        if (x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider != null)
+        {
+            moveDelta.x = 0;
+        }
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider != null) {
+            moveDelta.y = 0;
+        }
+
+        // Movement
+        transform.Translate(moveDelta * Time.deltaTime);
+    }
+}
